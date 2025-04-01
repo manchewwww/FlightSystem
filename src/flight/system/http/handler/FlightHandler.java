fe7dc2596ca2c074d4eaa3a16ad9a2d660c5f9c6
manchewwww <georgimanchev2003@gmail.com>
2025-04-02 00:01:31 +0300

    Add FlightHandler implementing HttpHandler for use in HTTPServer

---
+++ src/flight/system/http/handler/FlightHandler.java
@@ -0,0 +1,40 @@
+package flight.system.http.handler;
+
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import flight.system.data.Route;
+import flight.system.http.request.RouteRequest;
+import flight.system.http.service.FlightService;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.List;
+
+public class FlightHandler implements HttpHandler {
+
+    private static final String FILE_PATH = "src/flight/system/files/flights_example_2.csv";
+    private static final FlightService FLIGHT_SERVICE = new FlightService();
+    private static final Gson GSON = new Gson();
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        if ("POST".equals(exchange.getRequestMethod())) {
+            try (InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8")) {
+                RouteRequest request = GSON.fromJson(isr, RouteRequest.class);
+                List<Route> routes = FLIGHT_SERVICE.findRoutes(FILE_PATH, request.getOrigin(), request.getDestination());
+                routes =
+                    routes.stream().filter(route -> route.getCities().size() - 1 <= request.getMaxFlights()).toList();
+                String response = GSON.toJson(routes);
+
+                exchange.sendResponseHeaders(200, response.getBytes().length);
+                try (OutputStream os = exchange.getResponseBody()) {
+                    os.write(response.getBytes());
+                }
+            }
+        } else {
+            exchange.sendResponseHeaders(405, -1);
+        }
+    }
+}
