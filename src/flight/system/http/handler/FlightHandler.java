f14b9e2d8bc62b955e69e6c6112679b16367bff6
manchewwww <georgimanchev2003@gmail.com>
2025-04-02 00:13:00 +0300

    Replace magic numbers and strings with constants

--- src/flight/system/http/handler/FlightHandler.java
+++ src/flight/system/http/handler/FlightHandler.java
@@ -14,27 +14,34 @@ import java.util.List;
 
 public class FlightHandler implements HttpHandler {
 
+    private static final int ERROR_STATUS_CODE = 405;
+    private static final int RESPONSE_LENGTH = -1;
+    private static final int OK_STATUS_CODE = 200;
+    private static final String UTF = "utf-8";
     private static final String FILE_PATH = "src/flight/system/files/flights_example_2.csv";
+
     private static final FlightService FLIGHT_SERVICE = new FlightService();
     private static final Gson GSON = new Gson();
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         if ("POST".equals(exchange.getRequestMethod())) {
-            try (InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8")) {
+            try (InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), UTF)) {
                 RouteRequest request = GSON.fromJson(isr, RouteRequest.class);
-                List<Route> routes = FLIGHT_SERVICE.findRoutes(FILE_PATH, request.getOrigin(), request.getDestination());
+                List<Route> routes =
+                    FLIGHT_SERVICE.findRoutes(FILE_PATH, request.getOrigin(), request.getDestination());
                 routes =
                     routes.stream().filter(route -> route.getCities().size() - 1 <= request.getMaxFlights()).toList();
                 String response = GSON.toJson(routes);
 
-                exchange.sendResponseHeaders(200, response.getBytes().length);
+                exchange.sendResponseHeaders(OK_STATUS_CODE, response.getBytes().length);
                 try (OutputStream os = exchange.getResponseBody()) {
                     os.write(response.getBytes());
                 }
             }
         } else {
-            exchange.sendResponseHeaders(405, -1);
+            exchange.sendResponseHeaders(ERROR_STATUS_CODE, RESPONSE_LENGTH);
         }
     }
+
 }
