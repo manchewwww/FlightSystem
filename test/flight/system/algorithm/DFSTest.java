1f5d111d1d88dbad1b0032cdc7f22dd1d12220cd
manchewwww <georgimanchev2003@gmail.com>
2025-04-02 00:04:29 +0300

    Add unit tests for DFS

---
+++ test/flight/system/algorithm/DFSTest.java
@@ -0,0 +1,40 @@
+package flight.system.algorithm;
+
+import flight.system.data.Flight;
+import flight.system.data.Route;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DFSTest {
+
+    @Test
+    void testFindAllValidRoutes() {
+        List<Flight> flights = List.of(
+            new Flight("NYC", "CHI", 100.0),
+            new Flight("CHI", "LAX", 200.0),
+            new Flight("NYC", "LAX", 400.0)
+        );
+
+        List<Route> routes = DFS.findAllRoutes(flights, "NYC", "LAX");
+
+        assertEquals(2, routes.size(), "Invalid routes found. Program expect 2 routes!");
+        assertEquals(300.0, routes.get(0).getTotalPrice(), "First route price is calculated incorrect");
+        assertEquals(400.0, routes.get(1).getTotalPrice(), "Second route price is calculated incorrect");
+    }
+
+    @Test
+    void testWithNoValidRoute() {
+        List<Flight> flights = List.of(
+            new Flight("NYC", "CHI", 100.0)
+        );
+
+        List<Route> routes = DFS.findAllRoutes(flights, "NYC", "LAX");
+        assertTrue(routes.isEmpty(), "When no one route founded. Routes from DFS should be empty!");
+    }
+
+}
