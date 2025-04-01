2f06fb62c04bca90221d355f14628980d917d7bf
manchewwww <georgimanchev2003@gmail.com>
2025-04-01 23:54:49 +0300

    Add FlightService to load data and find all routes using DFS

---
+++ src/flight/system/http/service/FlightService.java
@@ -0,0 +1,14 @@
+package flight.system.http.service;
+
+import flight.system.algorithm.DFS;
+import flight.system.data.Route;
+import flight.system.repositories.FlightRepository;
+
+import java.util.List;
+
+public class FlightService {
+
+    public List<Route> findRoutes(String path, String origin, String destination) {
+        return DFS.findAllRoutes(FlightRepository.loadFlights(path), origin, destination);
+    }
+}
